b4cf9f6033c1416b8a1277a543e837e8cf3c4541
Mateusz <mtrx84@gmail.com>
2019-05-05 11:51:45 +0200

    Updates

---
+++ precache-manifest.c3efc5ee80d96d9a3067f47a08d8feb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "69944fb3518f0f0b5d2d89b2b106ce8e",
+    "url": "/spa/static/media/img3.69944fb3.jpg"
+  },
+  {
+    "revision": "94dc6ff3ba46eb46a38d6de09f4ed87e",
+    "url": "/spa/static/media/img2.94dc6ff3.jpg"
+  },
+  {
+    "revision": "3c7adf1a4d5fdcd2463db39713f2d722",
+    "url": "/spa/static/media/img1.3c7adf1a.jpg"
+  },
+  {
+    "revision": "2ed039b0821f278cb346",
+    "url": "/spa/static/js/runtime~main.2ed039b0.js"
+  },
+  {
+    "revision": "e7fa06ee4057b118fbc3",
+    "url": "/spa/static/js/main.e7fa06ee.chunk.js"
+  },
+  {
+    "revision": "f09c5eb13b38afe792af",
+    "url": "/spa/static/js/1.f09c5eb1.chunk.js"
+  },
+  {
+    "revision": "e7fa06ee4057b118fbc3",
+    "url": "/spa/static/css/main.3c7c0077.chunk.css"
+  },
+  {
+    "revision": "1534bff6703d4b896cd50047753fc143",
+    "url": "/spa/index.html"
+  }
+];
